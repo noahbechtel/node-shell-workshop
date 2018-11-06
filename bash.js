b8e6a82b2a908d25944c7b9504c3b94b6820f3b5
Noah <noahbehtel@gmail.com>
2018-11-06 11:16:44 -0600

    pwd functionality

---
+++ bash.js
@@ -0,0 +1,13 @@
+process.stdout.write('prompt > ');
+process.stdin.on('data',(data)=>{
+   
+    const cmd=data.toString().trim();
+    if(cmd==='pwd'){
+        pwd();
+    }
+
+
+    //process.stdout.write('\nprompt > ');
+});
+const pwd=require('./pwd');
+
