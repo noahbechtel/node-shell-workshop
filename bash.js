9e32c4d2866c15204f362f87d0e22d48607927de
Noah <noahbehtel@gmail.com>
2018-11-06 12:07:08 -0600

    new bash

--- bash.js
+++ bash.js
@@ -1,22 +1,20 @@
 process.stdout.write('prompt > ');
 process.stdin.on('data', data => {
   const cmd = data.toString().trim();
-
-  // if(cmd==='pwd'){
-  //     pwd();
-  // } else if ('cmd' )
-
-  switch (cmd) {
-    case 'pwd':
+  cmd=cmd.split(" ")
+  if(cmd[0]==='pwd'){
       pwd();
-      break;
-    case 'ls':
+  }
+  if (cmd[0]==='ls'){
       ls();
-      break;
-    default:
+  }
+  if(cmd[0]==='cat'){
+      cat(cmd[1]);
   }
 
   //process.stdout.write('\nprompt > ');
 });
 const pwd = require('./pwd');
 const ls = require('./ls');
+const cat = require('./cat')
+
